036fbbb5ee2ef1ad1539371b97f075e51a18aa4b
tikmart <t.martirosyan012@gmail.com>
2022-07-06 21:19:21 +0400

    Task 3 first commit

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,7 +2,9 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c = 10;
+        int a = 10;
+        int b = 10;
+        int c = 10;
 
         System.out.println(c);
     }
